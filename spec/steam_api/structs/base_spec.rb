bee523dddf0b684e1eb0d583dd2a73d274096ba2
João Pedro <pedrochaveslimas3@gmail.com>
2023-10-07 17:46:44 -0300

    Add base class struct specs

---
+++ spec/steam_api/structs/base_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+RSpec.describe SteamApi::Structs::Base do
+  let(:klass) do
+    Class.new(described_class) do
+      attribute :name, SteamApi::Structs::Types::String
+      attribute :options, SteamApi::Structs::Types::Hash.schema(
+        foo: SteamApi::Structs::Types::String.optional.default('Default foo'.freeze),
+        bar: SteamApi::Structs::Types::String
+      )
+    end
+  end
+
+  describe '.build_from_response' do
+    subject { klass.build_from_response(response) }
+
+    context 'when the response is valid' do
+      context 'and have no optional response params missing' do
+        let(:response) { { name: 'Steam', options: { foo: 'Custom foo', bar: 'Custom bar' } } }
+
+        it 'builds an instance with valid response' do
+          expect(subject).to be_a(klass)
+          expect(subject.name).to eq('Steam')
+          expect(subject.options).to eq({ foo: 'Custom foo', bar: 'Custom bar' })
+        end
+      end
+
+      context 'and have optional response params missing' do
+        let(:response) { {name: 'Gabe', options: { bar: 'Custom bar'} } }
+
+        it 'uses default values for missing response' do
+          expect(subject).to be_a(klass)
+          expect(subject.name).to eq('Gabe')
+          expect(subject.options).to eq({ foo: 'Default foo', bar: 'Custom bar' })
+        end
+      end
+    end
+
+    context 'when the response is invalid' do
+      let(:response) { { name: 'Newell', options: { baz: 'Invalid' } } }
+      it 'raises an error for invalid response' do
+        expect { subject }.to raise_error(Dry::Struct::Error)
+      end
+    end
+  end
+end
