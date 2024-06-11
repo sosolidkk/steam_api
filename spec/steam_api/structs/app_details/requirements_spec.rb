fb7e9547d31fdaf384adcb3cd98acc1b80bc77bf
João Pedro Chaves de Lima <pedrochaveslimas3@gmail.com>
2024-06-11 14:50:45 -0300

    Add requirements and screenshots specs

--- spec/steam_api/structs/app_details/requirements_spec.rb
+++ spec/steam_api/structs/app_details/requirements_spec.rb
@@ -3,5 +3,25 @@ require 'spec_helper'
 RSpec.describe SteamApi::Structs::AppDetails::Requirements do
   subject { described_class.new(params) }
 
-  pending 'TODO'
+  describe '#initialize' do
+    let(:params) do
+      {
+        minimum: minimum,
+        recommended: recommended
+      }
+    end
+
+    context 'when the params are present' do
+      let(:minimum) { 'Minimum: OS, RAM' }
+      let(:recommended) { 'Recommended: OS, RAM, HD' }
+
+      it { is_expected.to have_attributes(**params) }
+    end
+
+    context 'when the params are missing' do
+      let(:params) { {} }
+
+      it { is_expected.to have_attributes(minimum: nil, recommended: nil) }
+    end
+  end
 end
--- spec/steam_api/structs/app_details/screenshot_spec.rb
+++ spec/steam_api/structs/app_details/screenshot_spec.rb
@@ -3,5 +3,29 @@ require 'spec_helper'
 RSpec.describe SteamApi::Structs::AppDetails::Screenshot do
   subject { described_class.new(params) }
 
-  pending 'TODO'
+  describe '#initialize' do
+    let(:params) do
+      {
+        id: id,
+        path_thumbnail: path_thumbnail,
+        path_full: path_full
+      }
+    end
+
+    context 'when the params are present' do
+      let(:id) { 0 }
+      let(:path_thumbnail) { 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1174180/ss_66b553f4c209476d3e4ce25fa4714002cc914c4f.600x338.jpg?t=1714055653' }
+      let(:path_full) { 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1174180/ss_66b553f4c209476d3e4ce25fa4714002cc914c4f.1920x1080.jpg?t=1714055653' }
+
+      it { is_expected.to have_attributes(**params) }
+    end
+
+    context 'when the params are missing' do
+      let(:params) { {} }
+
+      it 'raises a Dry::Struct::Error' do
+        expect { subject }.to raise_error(Dry::Struct::Error)
+      end
+    end
+  end
 end
