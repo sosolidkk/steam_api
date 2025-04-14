f59f04f88d93c4a8e9a1996c107604227d3eda43
João Pedro <pedrochaveslimas3@gmail.com>
2025-04-13 22:54:32 -0300

    Add initial request spec

--- spec/steam_api/request_spec.rb
+++ spec/steam_api/request_spec.rb
@@ -1,7 +1,57 @@
 require "spec_helper"
+require "typhoeus"
 
 RSpec.describe SteamApi::Request do
-  subject(:instance) { described_class }
+  let(:request_struct) do
+    SteamApi::Structs::Requests::GetPlayerSummaries.new(steamids: [123])
+  end
 
-  pending "TODO"
+  let(:response_struct) { SteamApi::Structs::PlayerSummaries }
+  let(:client) do
+    SteamApi::Client.new(api_token: "test_api_key")
+  end
+
+  subject(:request) { described_class.new(request_struct: request_struct, response_struct: response_struct, client: client) }
+
+  describe "#initialize" do
+    it "builds the request without errors" do
+      expect { request }.not_to raise_error
+    end
+  end
+
+  describe "#run" do
+    let(:raw_response) { instance_double("Typhoeus::Response", body: "{}", code: 200) }
+
+    before do
+      allow(Typhoeus::Request).to receive(:new).and_return(instance_double("Typhoeus::Request", run: raw_response))
+      allow(SteamApi::FailureHandler.instance).to receive(:call)
+    end
+
+    it "executes the request and returns a response" do
+      response = request.run
+
+      expect(response).to be_a(SteamApi::Response)
+      expect(SteamApi::FailureHandler.instance).to have_received(:call).with(response)
+    end
+  end
+
+  describe "private methods" do
+    describe "#build_base_url" do
+      it "constructs the correct base URL" do
+        url = request.send(:build_base_url)
+        expect(url.to_s).to eq("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=test_api_key&steamids=123")
+      end
+    end
+
+    describe "#build_storefront_url" do
+      let(:request_struct) do
+        SteamApi::Structs::Requests::GetAppDetails.new(appid: 123, filters: [], cc: "US", l: "en")
+      end
+
+      it "constructs the correct storefront URL" do
+        url = request.send(:build_storefront_url)
+        expect(url.to_s).to eq("https://store.steampowered.com/api/appdetails/?cc=US&l=en&appids=123&filters=")
+      end
+    end
+  end
 end
